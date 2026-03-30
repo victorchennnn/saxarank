9820fc21598fd66d1ff2bae65dca7449f6827473
victorchennnn <158227727+victorchennnn@users.noreply.github.com>
2026-03-30 13:19:05 -0400

    Add Umami analytics tracking script

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>saxarank</title>
         <link rel="icon" type="image/x-icon" href="/pixil-frame-0.png" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="ec05cf61-7e0b-47a3-bf1b-402edfd533a9"></script>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
